bcc3efe2dca1bcba397114cafe52573d1bb29f69
Thomas Boutry <thomas.boutry@x3rus.com>
2016-01-11 17:03:56 -0500

    fix de la documentation pour docker avec l'ajout du TOC ainsi que la creation du script pour FIXER le probleme de layout pour moodle

---
+++ tools/fix_precode_moodle.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Description : Fix layout pour moodle 
+# 
+#   
+# 
+# Auteur : Boutry Thomas <thomas.boutry@x3rus.com>
+# Date de création : 2016-01-11
+# Licence : GPL v3.
+###############################################################################
+
+##########
+## VARS ##
+
+HTML_FILE=$1
+
+
+##########
+## MAIN ##
+
+sed -i  's/<pre><code>/<pre><span style="font-size: small;"><code>/g' $HTML_FILE 
+
