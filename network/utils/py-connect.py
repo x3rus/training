a62129aa84769480e11c7cbe5fff7b080a55cd04
Thomas Boutry <thomas.boutry@x3rus.com>
2016-08-25 08:22:03 -0400

    Ajout py-connect pour teste https

--- network/utils/py-connect.py
+++ network/utils/py-connect.py
@@ -1,12 +1,9 @@
+#!/usr/bin/python2
+#
+################################
 
->>> h2 = httplib.HTTPConnection('172.17.0.1')
->>> h2.request("GET","/")
->>> r1 = h2.getresponse()
->>> print r1.status, r1.reason
-200 OK
->>> h2 = httplib.HTTPConnection('172.17.0.1')
->>> h2.request("GET","/hejhdke")
->>> r1 = h2.getresponse()
->>> print r1.status, r1.reason
-404 Not Found
-
+import httplib
+h2 = httplib.HTTPConnection('www.x3rus.com')
+h2.request("GET","/kkjhk")
+r1 = h2.getresponse()
+print (r1.status, r1.reason)
