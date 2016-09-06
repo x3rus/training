403db0e7fe7618c3fe386147b763ae38654c314a
Thomas Boutry <thomas.boutry@x3rus.com>
2016-09-06 08:53:38 -0400

    Ajout information pour le SSL avec désactivation des cipher en probleme

---
+++ network/utils/py-connect-BAD.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2
+#
+################################
+
+import httplib
+import ssl
+
+# This restores the same behavior as before.
+context = ssl._create_unverified_context()
+
+h2 = httplib.HTTPSConnection('172.17.0.1',context=context)
+h2.request("GET","/API/de/la/MORT/super/compliquer")
+r1 = h2.getresponse()
+
+if r1.status == 200 :
+    print ("Super ca marche ")
+else :
+    print ("Nope, bonne chance :D")
+
