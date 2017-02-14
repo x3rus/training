741bfca5447ba805ae435060ceb218c79b767615
Thomas Boutry <thomas.boutry@x3rus.com>
2017-02-14 08:51:24 -0500

    Ajout des correctif pour le lien entre postfix et dovecot

--- serveur_de_courriels/dockers/srvMailWithImap/imap/run.sh
+++ serveur_de_courriels/dockers/srvMailWithImap/imap/run.sh
@@ -4,7 +4,7 @@ set -e # exit on error
 # Variables
 export LST_USERS=${LST_USERS:"NONE"}
 
-export LST_MAILS=${LST_MAILS:"/etc/dovecot/mails-$ACCEPT_DOMAIN.lst"}
+export LST_MAILS=${LST_MAILS:-"/etc/dovecot/mails.lst"}
 
 # Création des utilisateurs 
 if [ "$LST_USERS" == "NONE" ] ; then
@@ -14,12 +14,16 @@ fi
 
 # Creation des utilisateur 
 for user in $LST_USER ; do
-        adduser $user
-        # set password motpasse 
-        echo "${user}:motpasse" | chpasswd 
+        if ! getent passwd $user ; then
+            useradd $user
+            mkdir /home/$user
+            chown $user /home/$user
+            # set password motpasse 
+            echo "${user}:motpasse" | chpasswd 
 
-        # Création de la liste des adresses courriels valides 
-        echo "$user@$ACCEPT_DOMAIN OK" >> $LST_MAILS 
+            # Création de la liste des adresses courriels valides 
+            echo "$user@$ACCEPT_DOMAIN OK" >> $LST_MAILS 
+        fi
 done
 
 
