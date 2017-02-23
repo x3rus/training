04454f93872ca68af0923e21a24c648b4cc41def
Thomas Boutry <thomas.boutry@x3rus.com>
2017-02-23 08:44:08 -0500

    Création de la configuration de postfix admin et execution du setup

--- serveur_de_courriels/dockers/srvMailWithImapWithBD/http/run.sh
+++ serveur_de_courriels/dockers/srvMailWithImapWithBD/http/run.sh
@@ -2,19 +2,15 @@
 set -e # exit on error
 
 # Variables
-export EXT_RELAY_HOST=${EXT_RELAY_HOST:-"DIRECT"}
-export SMTP_HOSTNAME=${SMTP_HOSTNAME:-"relay.example.com"}
-export PERMIT_NETWORK=${PERMIT_NETWORK:-"192.168.0.0/16"}
-export ACCEPT_DOMAIN=${ACCEPT_DOMAIN:-"localhost $SMTP_HOSTNAME"}
-
-MYSQL_USER=${MYSQL_USER:-"myuser"}
-MYSQL_PASSWORD=${MYSQL_PASSWORD:-"superpass"}
-MYSQL_DATABASE=${MYSQL_DATABASE:-"mails"}
-DB_TYPE=${DB_TYPE:-"mysql"}
-MAIL_DOMAIN=${MAIL_DOMAIN:-"example.com"}
-SETUP_PASS=${SETUP_PASS:-"fkrhreiu"}
+export MYSQL_USER=${MYSQL_USER:-"myuser"}
+export MYSQL_PASSWORD=${MYSQL_PASSWORD:-"superpass"}
+export MYSQL_DATABASE=${MYSQL_DATABASE:-"mails"}
+export DB_TYPE=${DB_TYPE:-"mysql"}
+export MAIL_DOMAIN=${MAIL_DOMAIN:-"example.com"}
+export SETUP_PASS=${SETUP_PASS:-"fkrhreiu"}
 
 # Templates
 j2 /root/config.inc.php.j2 > /var/www/html/config.inc.php
 
-echo "toto" > /root/toto
+# Rappel de la commande CMD du script original
+apache2-foreground 
