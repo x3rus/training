05dff75f6760d1277b14e612fd4de734c0b7c87b
Thomas Boutry <thomas.boutry@x3rus.com>
2017-03-01 17:47:58 -0500

    finalisation du conteneur smtp mais il y a encore des prob dans l'image

--- serveur_de_courriels/dockers/srvMailWithImapWithBD/smtp/run.sh
+++ serveur_de_courriels/dockers/srvMailWithImapWithBD/smtp/run.sh
@@ -6,7 +6,7 @@ export EXT_RELAY_HOST=${EXT_RELAY_HOST:-"DIRECT"}
 export SMTP_HOSTNAME=${SMTP_HOSTNAME:-"relay.example.com"}
 export PERMIT_NETWORK=${PERMIT_NETWORK:-"192.168.0.0/16"}
 
-export DB_HOST=${DB_HOST:-"mysql"}
+export DB_HOST=${DB_HOST:-"db"}
 export DOVECOT_SRV=${DOVECOT_SRV:-"dovecot-lmtp" }
 
 echo $SMTP_HOSTNAME > /etc/mailname
@@ -14,8 +14,14 @@ echo $SMTP_HOSTNAME > /etc/mailname
 # Templates
 j2 /root/postfix-main.j2 > /etc/postfix/main.cf
 
-# Création du répertoire pour la configuration de sql
-mkdir /etc/postfix/sql/
+
+# génération des fichiers de configuration SQL pour postfix
+for sql_file in /root/sql/*.j2 ; do
+    filename=$(basename $sql_file) 
+    filedst="/etc/postfix/${filename::-3}"
+    j2 $sql_file > $filedst
+done
+
 
 # Launch
 rm -f /var/spool/postfix/pid/*.pid
