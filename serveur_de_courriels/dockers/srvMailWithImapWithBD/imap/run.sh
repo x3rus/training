259da13e3c626456fd9417b922e32034285c431a
Thomas Boutry <thomas.boutry@x3rus.com>
2017-03-03 17:23:37 -0500

    Finalisation des images ET fonctionnelle :P

--- serveur_de_courriels/dockers/srvMailWithImapWithBD/imap/run.sh
+++ serveur_de_courriels/dockers/srvMailWithImapWithBD/imap/run.sh
@@ -1,6 +1,13 @@
 #! /usr/bin/env bash
 set -e # exit on error
 
+# Variables
+export DB_HOST=${DB_HOST:-"db"}
+
+
+# Templates
+j2 /root/dovecot-sql.conf.ext.j2 > /etc/dovecot/dovecot-sql.conf.ext 
+
 # Launch
 rm -f /var/run/dovecot/*.pid  
 exec /usr/bin/supervisord -n
